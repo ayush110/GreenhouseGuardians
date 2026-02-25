434c709aa2f8764988e3430d6f71bf7e5444e03b
Kayen Mehta <kayenkmehta@gmail.com>
2026-02-25 12:57:27 -0500

    [Upload Script] Use MIME to pass content-type of image to backend API
    Summary: The API only accepts file types with images. We hadn't passed in
    content types into the file tuple, so it was sending the images with the
    default type: application/octet-stream. This resulted in a 400 Error: File must
    be an image (JPEG, PNG etc.).
    
    Using the mimetypes library, we grab the image type and pass it into the file
    tuple for the API.
    
    Testing: Tested locally on the Pi by updated the script and ensuring images
    were uploaded and then deleted.
    
    Ticket: n/a
    
    Reviewers: n/a

--- upload_service.py
+++ upload_service.py
@@ -1,6 +1,7 @@
 import requests
 from pathlib import Path
 import os
+import mimetypes
 
 API_URL = "https://deenp03-capstone-backend.hf.space/api/classify"
 IMAGE_DIR = Path("/home/pi/Documents/captures")
@@ -9,8 +10,9 @@ for image_path in IMAGE_DIR.glob("*.jpg"):
     print(f"Sending {image_path.name}...")
 
     try:
+        mime_type = mimetypes.guess_type(str(image_path))[0]
         with open(image_path, "rb") as f:
-            files = {"file": f}
+            files = {"file": (image_path.name, f, mime_type)}
             response = requests.post(API_URL, files=files)
 
         if response.status_code == 200:
